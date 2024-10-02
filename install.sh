a1529aa79bfe8f1668bec4813161787f9c90d455
Pedro Henrique Dos Santos <pedrosantosdevelop@gmail.com>
2024-10-02 11:29:57 -0300

    Update install.sh
    add docker

--- install.sh
+++ install.sh
@@ -17,6 +17,26 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code # or code-insiders
 
+# Docker 
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
 # sudo apt-get install android-sdk -y
 # echo 'export ANDROID_HOME="/usr/lib/android-sdk/"' >> "$HOME"/.bashrc
 # echo "export PATH='${PATH}:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/'" >> "$HOME"/.bashrc
